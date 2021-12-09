b8c7125f14e0e56a3d6b917e725fbbe960791b3e
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-09 15:19:08 +0530

    Added: First Servlet

---
+++ src/main/java/com/bridgelabs/MyFirstServlet.java
@@ -0,0 +1,27 @@
+package com.bridgelabs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+
+public class MyFirstServlet extends HttpServlet {
+
+
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3> Hello World My First Servlet </h3>");
+		out.close();
+	}
+
+
+}
