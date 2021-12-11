981c71deaf1eab6db95f07101336f866d4122a1b
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-11 08:01:10 +0530

    Added:Abilty Enter a valid password

--- src/main/java/com/bridgelabs/LoginServlets.java
+++ src/main/java/com/bridgelabs/LoginServlets.java
@@ -1,5 +1,5 @@
 package com.bridgelabs;
-
+import java.lang.NullPointerException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.regex.Matcher;
@@ -24,29 +24,25 @@ import jakarta.servlet.annotation.WebServlet;
 		urlPatterns = {"/LoginServlets"},
 		initParams = {
 				@WebInitParam(name="user", value="Srikanth"),
-				@WebInitParam(name="pwd",value = "Srikanth@534")
+				@WebInitParam(name="password",value = "Srikanth@534")
 		}
 		)
 public class LoginServlets extends HttpServlet {
 
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws NullPointerException,ServletException, IOException {
 		// get Request parameters for userId and Password
 		//Regex pattern for user name
 		String user = request.getParameter("user");
 		String NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
-		Pattern p = Pattern.compile(NAME_PATTERN);
-		Matcher m = p.matcher(user);
 		//Regex pattern for password
 		String pwd = request.getParameter("pwd");
 		String PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{8,}$";
-	    Pattern p2 = Pattern.compile(PASSWORD);
-	    Matcher m2 = p2.matcher(pwd);
 		// get servlet configuration init parameters
-		String userId = getServletConfig().getInitParameter("userId");
+		String userId = getServletConfig().getInitParameter("user");
 		String password = getServletConfig().getInitParameter("password");
-		if (userId.equals(user) && password.equals(pwd)) {
+		if (userId.equals(user) && userId.matches(NAME_PATTERN)&&password.equals(pwd)&&password.matches(PASSWORD)) {
 			request.setAttribute("user", user);
 			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
 		} else {
@@ -56,6 +52,8 @@ public class LoginServlets extends HttpServlet {
 			rd.include(request, response);
 
 		}
+
+
 
 	}
 }
